170fdc574b55c98044ff4c61feaa953c5ddb0329
Ali Rhayem <alirhayem.1212@gmail.com>
2024-12-28 17:48:42 +0200

    add loading indicator to button in NewItem widget during item submission

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -165,7 +165,13 @@ class _NewItemState extends State<NewItem> {
                   ),
                   ElevatedButton(
                     onPressed: _isSending ? null : _saveItem,
-                    child: Text('Add Item'),
+                    child: _isSending
+                        ? SizedBox(
+                            height: 16,
+                            width: 16,
+                            child: CircularProgressIndicator(),
+                          )
+                        : Text('Add Item'),
                   ),
                 ],
               ),
